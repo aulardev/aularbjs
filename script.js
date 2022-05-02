2ffc04389e388351f196d619c007b5912653b4aa
Cesar Aular <cesar.aularb@gmail.com>
2022-05-01 23:18:03 -0300

    nuevos cambios

--- script.js
+++ script.js
@@ -1,38 +1,84 @@
-
-// funcion para calcular costo total del viaje // 
-
-const Presupuesto = 6000
-
-let entrada = prompt ("Ingrese su nombre");
-
-while (entrada !="ESC") {
-    alert ("Nombre registrado:" + entrada);
-
-    entrada = prompt ("Ingrese su apellido");
+class Producto {
+    constructor(id, nombre, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
 
-let preciodelseguro = parseFloat(prompt("Ingrese precio del seguro"))
-let preciodelpasaje = parseFloat(prompt("Ingrese gasto del pasaje"))
-let preciodelhospedaje = parseFloat(prompt("Ingrese gasto del hospedaje"))
-let preciodeltransporte = parseFloat(prompt("Ingrese gasto del transporte"))
-let operacion = prompt("Ingrese una operacion (+)")
- 
-
-if(operacion == "+") {
-    console.log(preciodelseguro + preciodelpasaje + preciodelhospedaje + preciodeltransporte)
-}
+const alimento = new Producto(1, "alimento", 900)
+const comedero = new Producto(2, "comedero", 400)
+const snacks = new Producto(3, "snacks", 500)
+const pipetas = new Producto(4, "pipetas", 350)
+const piedras = new Producto(5, "piedras", 200)
+const camas = new Producto(6, "camas", 5000)
 
 
-else {
-    console.log("operacion no valida")
-}
+const productos = [alimento, comedero, snacks, pipetas, piedras, camas]
 
 
+const carrito = [];
 
-function suma(seguro, pasaje, hospedaje, transporte) {
-    return seguro + pasaje + hospedaje + transporte
+let productoElegido;
+
+const container = document.querySelector(".container");
+
+// function saludo(){}
+
+const saludar = () => {
+    alert("Hola! Bienvenido a Intermascota")
+    let nombre = prompt("Ingrese su nombre")
+    while (!isNaN(nombre)) {
+        nombre = prompt("Ingrese correctamente su nombre")
+    }
+    container.innerHTML = `<h1>Bienvenido ${nombre.toUpperCase()}</h1>`
+};
+
+const consultarProductos = () => {
+    let texto = "";
+    for (const p of productos) {
+        texto += `${p.id}) ${p.nombre}\n`;
+    }
+    let prod = parseInt(prompt(`Que producto quiere llevar? :\n${texto} `));
+
+    while (prod >6 || prod < 1 || isNaN(prod)) {
+        prod = parseInt(prompt(`Que producto quiere llevar? :\n${texto} `));
+    }
+
+    return prod;
+};
+
+const llevarProducto = () => {
+    let buscarProducto = productos.find(
+        (element) => element.id === productoElegido
+    );
+
+    let existe = carrito.some((element) => element.id === productoElegido);
+
+    if (existe) {
+        buscarProducto.cantidad++;
+    } else {
+        buscarProducto.cantidad = 1;
+        carrito.push(buscarProducto);
     }
 
 
-    
+    const seguir = confirm("Quiere agregar otro producto?")
+
+    if (seguir) {
+        productoElegido = consultarProductos();
+        llevarProducto();
+    }
+};
+
+saludar();
+productoElegido = consultarProductos();
+llevarProducto();
+
+
+
+
+
+
+
 
